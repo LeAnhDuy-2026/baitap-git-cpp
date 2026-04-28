7938f3b6d530dd4b69c0e44852913bfa8906363c
LeAnhDuy-2026 <duytrm2019@gmail.com>
2026-04-28 10:27:47 +0700

    Add tính dt hình tròn

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double banKinh, dienTich;
+
+    cout << "Nhap ban kinh: ";
+    cin >> banKinh;
+
+    dienTich = M_PI * pow(banKinh, 2);
+
+    cout << "Dien tich hinh tron la: " << dienTich << endl;
+
+    return 0;
+}
